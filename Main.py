43d3a4728bc89ca25b65248574970c82c389ee16
BeckSM64 <becksm64@gmail.com>
2021-11-10 18:31:48 -0500

    :sparkles: add back command line option

--- Main.py
+++ Main.py
@@ -9,58 +9,59 @@ from RandomizerGUI import RandomizerGUI
 
 def main():
 
-    gui = RandomizerGUI()
+    # tools
+    n64converter = "N64RomConverter.py"
+    n64checksum = "n64cksum.py"
 
-    # # tools
-    # n64converter = "N64RomConverter.py"
-    # n64checksum = "n64cksum.py"
+    if len(sys.argv) <= 1:
+        RandomizerGUI()
 
-    # # check number of args
-    # if len(sys.argv) != 2:
-    #     sys.exit(
-    #         "Incorrect number of arguments. Usage: python3 bherorandomizer.py <input_file>"
-    #     )
+    else:
+        
+        # generate seed
+        seed = random.randint(0, 1000000)
 
-    # # get file as input
-    # input_name = sys.argv[1]
-    # output_name = input_name
+        # get file as input
+        input_name = sys.argv[1]
+        output_name = input_name
 
-    # # help
-    # if (sys.argv[1] == "-h") or (sys.argv[1] == "-help"):
-    #     sys.exit(
-    #         "Input must be a Bomberman Hero ROM file with .n64 extension. \n Usage: python3 bherorandomizer.py <input_file>"
-    #     )
+        # help
+        if (sys.argv[1] == "-h") or (sys.argv[1] == "-help"):
+            sys.exit(
+                "Input must be a Bomberman Hero ROM file with .n64 extension. \n Usage: python3 bherorandomizer.py <input_file>"
+            )
 
-    # # check file extension
-    # if ".n64" not in input_name:
-    #     sys.exit("Invalid input file. Make sure the file extension is .n64")
+        # check file extension
+        if ".n64" not in input_name:
+            sys.exit("Invalid input file. Make sure the file extension is .n64")
 
-    # # ensure dependencies are in directory
-    # if not os.path.exists(n64converter):
-    #     sys.exit("Cannot locate " + n64converter + ". Exiting...")
-    # if not os.path.exists(n64checksum):
-    #     sys.exit("Cannot locate " + n64checksum + ". Exiting...")
+        # ensure dependencies are in directory
+        if not os.path.exists(n64converter):
+            sys.exit("Cannot locate " + n64converter + ". Exiting...")
+        if not os.path.exists(n64checksum):
+            sys.exit("Cannot locate " + n64checksum + ". Exiting...")
 
-    # # change file extension for ouput file
-    # output_name = output_name.replace(".n64", ".z64")
+        # setup output name
+        output_name = output_name.split(".")[0] + ".rando.z64"
 
-    # # N64CONVERTER -i [INPUT] -o [OUTPUT]
-    # subprocess.call(["python", n64converter, "-i", input_name, "-o", output_name])
+        # N64CONVERTER -i [INPUT] -o [OUTPUT]
+        subprocess.call(["python", n64converter, "-i", input_name, "-o", output_name])
 
-    # # generate the new level order dictionary
-    # randomize_stages()
+        # generate the new level order dictionary
+        randomize_stages(seed)
 
-    # # hold the bytes read in from the rom file
-    # rom_data = read_file(output_name)
+        # hold the bytes read in from the rom file
+        rom_data = read_file(output_name)
 
-    # # write modified data back to rom
-    # write_file(output_name, rom_data)
+        # write modified data back to rom
+        write_file(output_name, rom_data)
 
-    # # recalculate checksum
-    # subprocess.call(["python", n64checksum, output_name])
+        # recalculate checksum
+        subprocess.call(["python", n64checksum, output_name])
 
-    # # print success
-    # print("Success. Generated output file " + output_name + " in current directory")
+        # print success
+        print("Success. Generated output file " + output_name + " in current directory")
+        print("Seed: " + str(seed))
 
 
 if __name__ == "__main__":
