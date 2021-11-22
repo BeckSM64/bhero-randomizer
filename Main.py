f1fbe03f7af3f123f4803f202aed61d4228b3e8e
BeckSM64 <becksm64@gmail.com>
2021-11-22 17:13:32 -0500

    :sparkles: add input rom and output dir args

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 import sys
 import random
-import subprocess
-import os.path
+import argparse
 from RomData import *
 from BheroRandomizer import *
 from RandomizerGUI import RandomizerGUI
@@ -9,36 +8,31 @@ from RandomizerGUI import RandomizerGUI
 
 def main():
 
-    # tools
-    n64converter = "N64RomConverter.py"
-
     if len(sys.argv) <= 1:
         RandomizerGUI()
 
     else:
+
+        parser = argparse.ArgumentParser()
+        parser.add_argument("input_rom", help="The clean Bomberman Hero ROM", type=str)
+        parser.add_argument("output_dir", help="The directory to place the generated ROM", type=str)
+        args = parser.parse_args()
         
         # generate seed
         seed = random.randint(0, 1000000)
 
         # get file as input
-        input_name = sys.argv[1]
+        input_name = args.input_rom
 
-        # help
-        if (sys.argv[1] == "-h") or (sys.argv[1] == "-help"):
-            sys.exit(
-                "Input must be a Bomberman Hero ROM file with .n64 extension. \n Usage: python3 bherorandomizer.py <input_file>"
-            )
+        # get output dir
+        output_dir = args.output_dir
 
         # check file extension
         if ".n64" not in input_name:
             sys.exit("Invalid input file. Make sure the file extension is .n64")
 
-        # ensure dependencies are in directory
-        if not os.path.exists(n64converter):
-            sys.exit("Cannot locate " + n64converter + ". Exiting...")
-
         # generate the new level order dictionary
-        generate_rom(input_name, ".", seed)
+        generate_rom(input_name, output_dir, seed)
 
         # print success
         print("Seed: " + str(seed))
